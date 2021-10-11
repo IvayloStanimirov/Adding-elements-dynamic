c5163909586a0d4a0529689ca56595d8cd154dca
Ivaylo <kolevivaylo27@gmail.com>
2021-10-11 20:23:20 +0300

    Adding dynamic articles.

--- src/js/app.js
+++ src/js/app.js
@@ -7,4 +7,32 @@ window.addEventListener("DOMContentLoaded", () => {
   button.addEventListener("click", () => {
     alert("💣");
   });
+
+  function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+
+
+  let body = document.querySelector('body');
+
+  body.addEventListener('click', function(){
+    // let main = document.querySelector('.main')
+
+    for(let i = 0; i < 5; i++){
+      let article = document.createElement('article');
+      let articleText = makeid(5)
+      article.innerText = articleText;
+      article.classList.add('message')
+      body.prepend(article);
+    }
+  })
+
 });
